ec3632fe48c8962db42ed886d83585f17ec7228b
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-08-10 11:08:35 +0500

    login endpoint with bcrypt and JWT token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,56 @@
+require('dotenv').config();
 const express =  require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 
-dotenv.config();
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
         const user = await User.find();
-        const data = {
-            id: user[0].id
-        }
-
-        const token = jwt.sign(data, process.env.SECRET_KEY);
-        console.log(token); 
         res.json(user);
     } catch (err) {
         res.json(err);
     }
 })
+
+router.post('/login', [
+    body('email', 'Invalid Email').isEmail(),
+    body('password', 'Invalid Password').isLength({ min: 6 })
+], async (req, res) => {
+    const err = validationResult(req);
+    if(!err.isEmpty()) {
+        return res.status(400).json({ err: err.array() });
+    } else {
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ error: "No such user exists" });
+            }
+
+            const passCompare = await bcrypt.compare(password, user.password);
+            if (!passCompare) {
+                return res.status(400).json({ error: "Password doesn't match" });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const token = jwt.sign(data, process.env.SECRET_KEY);
+            res.json({ token });
+
+        } catch (err) {
+            res.status(500).send("Internal Server Error")
+        }
+    }
+
+});
 
 router.post('/createuser', [
     body('name', 'Invalid Name').isLength({ min: 3, max: 15 }),
@@ -47,7 +76,7 @@ router.post('/createuser', [
                 res.json({ response: "Couldn't create user", error: "Email already in use"});
             }
         } catch (err) {
-            res.err(err.message);
+            res.status(500).send("Internal Server Error")
         }
     }
 });
