b04c0ded1032c81821f8ab1349656e118260ace7
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-04-20 21:55:36 +0500

    change method to PUT

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,11 +48,10 @@ router.delete('/:id', authorize, async (req, res) => {
 });
 
 //to update a note
-router.patch('/:id', authorize, async (req, res) => {
+router.put('/:id', authorize, async (req, res) => {
   try {
     const { id } = req.params;
     await Note.findOneAndUpdate({ user: req.user?.id, _id: id }, req.body);
-    await Note.replaceOne({ user: req.user?.id, _id: id }, req.body);
     res.status(200).send('Note Updated');
   } catch (err) {
     res.status(500).json({Error: 'Internal Server Error'});
