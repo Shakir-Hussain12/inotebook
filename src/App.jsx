2b818e812b1d538fcde9a40d14552fd013a9a1d5
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-04-23 13:30:37 +0500

    Removed unwanted files & initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,17 @@
+import { Router } from 'react-router-dom';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/auth').then((res) => res.json()).then((ans) => {
-      setData(ans);
-    });
-  }, []);
-
-  let key = 'abca1';
   return (
-    <div className="App">
-      <header className="App-header" style={{ display: 'flex', flexDirection: 'row' }}>
-        {
-          data.length !== 0 ? (data.map((person) => {
-            key += 1;
-            return (
-              <div
-                key={key}
-                style={
-                {
-                  border: '1px solid red',
-                  padding: '2%',
-                  marginInlineEnd: '2%',
-                  wordBreak: 'break-word',
-                }
-              }
-              >
-                <p>{person.full_name}</p>
-                <p>{person.name}</p>
-                <p>{person.email}</p>
-                <p>{person.password}</p>
-                <p>{person.phone_no}</p>
-              </div>
-            );
-          })) : (<p>No data</p>)
-        }
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
+    </>
   );
 }
 
