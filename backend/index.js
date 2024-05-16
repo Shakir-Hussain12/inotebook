68c7ce0e3780127a244aeb5f7980c022bb499e9e
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-16 22:41:11 +0500

    updated CORS policies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const db = require('./db');
 
 const port = 5000;
@@ -7,13 +6,28 @@ const app = express();
 app.use(express.json());
 db();
 
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000']; // Add other origins as needed
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,auth-token');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Entry point
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
 // App routes
-app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
