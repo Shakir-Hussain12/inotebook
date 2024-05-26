9a850c165e7d851379d5cf2cbbd2a2a69feb3dc8
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-26 12:07:50 +0500

    Code to intercept axios requests & add bearer token

---
+++ AxiosInterceptor.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/',
+});
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('token');
+  const newConfig = { ...config };
+
+  if (token) {
+    newConfig.headers = {
+      ...newConfig.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return newConfig;
+}, (error) => Promise.reject(error));
+
+export default api;
