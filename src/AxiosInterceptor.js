5f4e6b3e87599b308f6e89bec7d6a96c82b8f034
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-26 19:26:57 +0500

    Refactoring

--- src/AxiosInterceptor.js
+++ src/AxiosInterceptor.js
@@ -1,22 +1,30 @@
-// import axios from 'axios';
-// import Cookies from 'js-cookie';
+import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000/',
-// });
+const api = axios.create({
+  baseURL: 'http://localhost:5000/',
+});
 
-// api.interceptors.request.use((config) => {
-//   const token = Cookies.get('token');
-//   const newConfig = { ...config };
+api.interceptors.response.use((response) => console.log(response),
+  (error) => {
+    if (error.response.status === 401) {
+      console.log('Unauthorized');
+    }
 
-//   if (token) {
-//     newConfig.headers = {
-//       ...newConfig.headers,
-//       Authorization: `Bearer ${token}`,
-//     };
-//   }
+    return Promise.reject(error);
+  });
 
-//   return newConfig;
-// }, (error) => Promise.reject(error));
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('token');
+  const newConfig = { ...config };
 
-// export default api;
+  if (token) {
+    newConfig.headers = {
+      ...newConfig.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return newConfig;
+}, (error) => Promise.reject(error));
+
+export default api;
