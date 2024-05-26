c15033ecc3f8ed0e5d40cec75e2624705cc26c17
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-26 12:52:28 +0500

    Added middleware for cookies

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const db = require('./db');
 
 const port = 5000;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 db();
 
 app.use((req, res, next) => {
