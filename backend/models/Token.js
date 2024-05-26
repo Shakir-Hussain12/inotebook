886f2d58903f90342f066b29066501f4cbeaf227
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-26 13:13:14 +0500

    Added Model for stroing refresh tokens

---
+++ backend/models/Token.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const tokenSchema = new Schema({
+    token: String,
+    userId: String,
+});
+
+module.exports = mongoose.model('Token', tokenSchema);
