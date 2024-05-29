f1a92a3a9d5d5a134d3cbc5f00b992adb2c26d54
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-05-29 11:23:17 +0500

    Created axios response interceptor

--- src/AxiosInterceptor.js
+++ src/AxiosInterceptor.js
@@ -1,30 +1,23 @@
 import axios from 'axios';
+// import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/',
 });
 
-api.interceptors.response.use((response) => console.log(response),
+api.interceptors.response.use(
+  (response) => {
+    console.log('Response was received');
+    console.log(response.data);
+    return response;
+  },
   (error) => {
-    if (error.response.status === 401) {
-      console.log('Unauthorized');
+    if (error.response && error.response.status === 401) {
+      // Handle 401 errors (unauthorized)
+      console.error('Unauthorized access - redirecting to login');
+      // You can perform a redirect to login page here or any other action
     }
-
     return Promise.reject(error);
-  });
-
-api.interceptors.request.use((config) => {
-  const token = Cookies.get('token');
-  const newConfig = { ...config };
-
-  if (token) {
-    newConfig.headers = {
-      ...newConfig.headers,
-      Authorization: `Bearer ${token}`,
-    };
-  }
-
-  return newConfig;
-}, (error) => Promise.reject(error));
-
+  },
+);
 export default api;
