8fe676fa249d782acd7483c88a8cc3cbc259afab
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-06-04 12:25:42 +0500

    Added initial tests for the app

---
+++ src/__tests__/auth.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../Redux/store';
+import NoteState from '../context/notes/noteState';
+import Home from '../pages/Home';
+
+describe('Home Page', () => {
+  test('renders Home page', () => {
+    render(
+      <Provider store={store}>
+        <NoteState>
+          <BrowserRouter>
+            <Home />
+          </BrowserRouter>
+        </NoteState>
+      </Provider>,
+    );
+    const linkElement = screen.getByText(/My Notes/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
