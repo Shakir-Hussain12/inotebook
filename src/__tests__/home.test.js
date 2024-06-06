b3ac0a666f3815f630ec6ec3b5a1575c10a1738e
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-06-06 20:05:47 +0500

    Added more tests for home page

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -6,7 +6,7 @@ import NoteState from '../context/notes/noteState';
 import Home from '../pages/Home';
 
 describe('Home Page', () => {
-  test('renders Home page', () => {
+  it('renders Home page', () => {
     render(
       <Provider store={store}>
         <NoteState>
@@ -18,5 +18,19 @@ describe('Home Page', () => {
     );
     const spanElement = screen.getByText(/My Notes/i);
     expect(spanElement).toBeInTheDocument();
+  });
+
+  it('contains a button with text Add a Note', () => {
+    render(
+      <Provider store={store}>
+        <NoteState>
+          <BrowserRouter>
+            <Home />
+          </BrowserRouter>
+        </NoteState>
+      </Provider>,
+    );
+    const buttonElement = screen.getByRole('button', { name: /Add a new Note/i });
+    expect(buttonElement).toBeInTheDocument();
   });
 });
