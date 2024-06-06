3cadd245ac2b4687d3707f1063dcef3799cafff1
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-06-06 20:06:09 +0500

    Added more tests for authentication including events

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import NoteState from '../context/notes/noteState';
@@ -6,7 +6,86 @@ import store from '../Redux/store';
 import Authenticate from '../pages/Authenticate';
 
 describe('Authenticate Page', () => {
-  test('renders Auth page', () => {
+  it('renders Auth page', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NoteState>
+            <Authenticate />
+          </NoteState>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const headerElement = screen.getByRole('heading');
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it('contains a description', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NoteState>
+            <Authenticate />
+          </NoteState>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const spanElement = screen.getByText(/Your personal Notebook/i);
+    expect(spanElement).toBeInTheDocument();
+  });
+
+  // it('contains a login form', () => {
+  //   render(
+  //     <Provider store={store}>
+  //       <BrowserRouter>
+  //         <NoteState>
+  //           <Authenticate />
+  //         </NoteState>
+  //       </BrowserRouter>
+  //     </Provider>,
+  //   );
+  //   const formElement = screen.getByRole('form');
+  //   expect(formElement).toBeInTheDocument();
+  // });
+
+  it('form contains 2 input fields if logging in', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NoteState>
+            <Authenticate />
+          </NoteState>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const emailField = screen.getByText(/Email/i);
+    const passwordField = screen.getByText(/Password/i);
+    expect(emailField && passwordField).toBeInTheDocument();
+  });
+
+  it('form contains 4 fields if registering', async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NoteState>
+            <Authenticate />
+          </NoteState>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const passwordField = screen.getByText(/Password/i);
+
+    await act(async () => {
+      const toggleButton = screen.getByRole('button', { name: /register/i });
+      fireEvent.click(toggleButton);
+    });
+
+    const inputField = screen.getAllByRole('textbox');
+    expect(inputField.length).toBe(3);
+    expect(passwordField).toBeInTheDocument();
+  });
+
+  it('contains a login button', () => {
     render(
       <Provider store={store}>
         <BrowserRouter>
@@ -17,6 +96,20 @@ describe('Authenticate Page', () => {
       </Provider>,
     );
     const buttonElement = screen.getByText(/Login/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('has button to registration', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NoteState>
+            <Authenticate />
+          </NoteState>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const buttonElement = screen.getByRole(('button'), { name: /register/i });
     expect(buttonElement).toBeInTheDocument();
   });
 });
