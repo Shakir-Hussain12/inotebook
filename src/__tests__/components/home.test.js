6f3c1c73079eea3f172c9642946a9e35f5ff7fc3
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-06-21 00:56:44 +0500

    Integration test for NoteItem & mocked API calls

--- src/__tests__/components/home.test.js
+++ src/__tests__/components/home.test.js
@@ -3,42 +3,98 @@ import {
 } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import api from '../../AxiosInterceptor';
 import store from '../../Redux/store';
 import NoteState from '../../context/notes/noteState';
 import Home from '../../pages/Home';
 
 describe('Home Page', () => {
-  beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <NoteState>
-          <BrowserRouter>
-            <Home />
-          </BrowserRouter>
-        </NoteState>
-      </Provider>,
-    );
+  describe('Unit Tests', () => {
+    beforeEach(() => {
+      render(
+        <Provider store={store}>
+          <NoteState>
+            <BrowserRouter>
+              <Home />
+            </BrowserRouter>
+          </NoteState>
+        </Provider>,
+      );
+    });
+
+    afterEach(() => {
+      cleanup();
+    });
+
+    it('renders Home page', () => {
+      const spanElement = screen.getByText(/My Notes/i);
+      expect(spanElement).toBeInTheDocument();
+    });
+
+    it('contains a button with text Add a Note', () => {
+      const buttonElement = screen.getByRole('button', { name: /Add a new Note/i });
+      expect(buttonElement).toBeInTheDocument();
+    });
+
+    it('shows form when clicked on Add a new Note button', async () => {
+      const formButton = screen.getByRole('button', { name: /Add a new Note/i });
+      fireEvent.click(formButton);
+
+      const formElement = await screen.findByTestId('noteForm');
+      expect(formElement).toBeInTheDocument();
+    });
   });
 
-  afterEach(() => {
-    cleanup();
-  });
+  describe('Note Item Integration Tests', () => {
+    const renderFunc = () => {
+      render(
+        <Provider store={store}>
+          <NoteState>
+            <BrowserRouter>
+              <Home />
+            </BrowserRouter>
+          </NoteState>
+        </Provider>,
+      );
+    };
 
-  it('renders Home page', () => {
-    const spanElement = screen.getByText(/My Notes/i);
-    expect(spanElement).toBeInTheDocument();
-  });
+    it('renders the note item component', async () => {
+      jest.spyOn(api, 'get').mockResolvedValueOnce({
+        data: [
+          {
+            _id: '1',
+            title: 'Very First Note',
+            description: 'Description of Note 1',
+            tag: 'General',
+          },
+          {
+            _id: '2',
+            title: 'Second Note',
+            description: 'Description of Note 2',
+            tag: 'Specific',
+          },
+        ],
+      });
 
-  it('contains a button with text Add a Note', () => {
-    const buttonElement = screen.getByRole('button', { name: /Add a new Note/i });
-    expect(buttonElement).toBeInTheDocument();
-  });
+      renderFunc();
 
-  it('shows form when clicked on Add a new Note button', async () => {
-    const formButton = screen.getByRole('button', { name: /Add a new Note/i });
-    fireEvent.click(formButton);
+      const noteItem = await screen.findByText(/Description of note 1/i);
+      expect(noteItem).toBeInTheDocument();
 
-    const formElement = await screen.findByTestId('noteForm');
-    expect(formElement).toBeInTheDocument();
+      cleanup();
+    });
+
+    it('renders the error message when no notes are found', async () => {
+      jest.spyOn(api, 'get').mockRejectedValueOnce({
+        message: 'No Notes Found',
+      });
+
+      renderFunc();
+
+      const errorMessage = await screen.findByText(/No Notes Found/i);
+      expect(errorMessage).toBeInTheDocument();
+
+      cleanup();
+    });
   });
 });
