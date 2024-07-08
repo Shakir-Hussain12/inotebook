c2e4a3d3725aacef58353410697e13c9d6a6d218
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-07-09 00:08:37 +0500

    mocking GET & POST requests fro registration

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -13,12 +13,31 @@ describe('Authentication tests', () => {
   });
 
   it('redirects to login upon successful registration', () => {
+    cy.intercept('GET', 'http://localhost:5000/api/auth/', {
+      statusCode: 200,
+      body: [],
+    }).as('mockGetResponse');
+
+    cy.intercept('POST', 'http://localhost:5000/api/auth/createuser', {
+      statusCode: 200,
+      body: { message: 'User created' },
+    }).as('mockPostResponse');
+
     cy.get('[name=toggleButton]').click();
     cy.get('[data-testid="first-name"]').type('John');
     cy.get('[data-testid="last-name"]').type('Cena');
     cy.get('[data-testid="email"]').type('johndoe@gmail.com');
     cy.get('[data-testid="password"]').type('123456');
     cy.get('[name=submitButton]').click();
+
+    cy.wait(['@mockGetResponse', '@mockPostResponse']).then(([getInterceptor, postInterceptor]) => {
+      expect(getInterceptor.response.statusCode).to.eq(200);
+
+      expect(postInterceptor.response.statusCode).to.eq(200);
+      expect(postInterceptor.response.body).to.have.property('message', 'User created');
+    });
+
+    cy.get('[name=submitButton]').contains('Login').should('exist');
   });
 
 });
