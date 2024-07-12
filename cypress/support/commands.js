d2be81828dad4a299d274f8579afda3e463d45dc
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-07-12 13:20:42 +0500

    Added custom logout command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
+/* eslint-disable no-undef */
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.intercept('POST', 'http://localhost:5000/api/auth/login', {
+    statusCode: 200,
+    body: { message: 'Logged In Successfully' },
+  }).as('mockPostResponse');
+
+  cy.get('[data-testid="email"]').type(email);
+  cy.get('[data-testid="password"]').type(password);
+  cy.get('[name=submitButton]').click();
+
+  cy.wait('@mockPostResponse');
+});
