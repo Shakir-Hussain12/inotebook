44b012480d25c2c2990c5303b5deef6204fd2d9c
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-07-12 13:24:17 +0500

    additional tests when adding a new note

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -47,4 +47,52 @@ describe('Home Page tests', () => {
     cy.get('[value="First Note"]').should('exist');
     cy.get('[value="Second Note"]').should('exist');
   });
+
+  it('adds a new note on the home page', () => {
+    cy.intercept('GET', 'http://localhost:5000/api/notes', {
+      statusCode: 200,
+      body: [],
+    }).as('mockGetResponse');
+
+    cy.intercept('POST', 'http://localhost:5000/api/notes', {
+      statusCode: 201,
+      body: {
+        note: {
+          tag: 'Work',
+          title: 'New Note',
+          description: 'This is the new note',
+        },
+      },
+    }).as('mockPostResponse');
+
+    cy.wait('@mockGetResponse');
+
+    cy.get('[name="addNote"]').click();
+    cy.get('#Tag').select('Work');
+    cy.get('[data-testid="title"]').type('New Note');
+    cy.get('[data-testid="description"]').type('This is the new note');
+    cy.get('[name="saveFormButton"]').click();
+
+    cy.wait('@mockPostResponse').then((interceptor) => {
+      expect(interceptor.response.statusCode).to.eq(201);
+    });
+
+    cy.get('[value="New Note"]').should('exist');
+    cy.contains('This is the new note').should('exist');
+  });
+
+  it('goes back to home page without adding a note upon cancelling', () => {
+    cy.intercept('GET', 'http://localhost:5000/api/notes', {
+      statusCode: 200,
+      body: [],
+    }).as('mockGetResponse');
+
+    cy.wait('@mockGetResponse');
+
+    cy.get('[name="addNote"]').click();
+    cy.get('[name="cancelButton"]').click();
+
+    cy.get('[name="addNote"]').should('exist');
+    cy.get('[data-testid="noteItem"]').should('not.exist');
+  });
 });
