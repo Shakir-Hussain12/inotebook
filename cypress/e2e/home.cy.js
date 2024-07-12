36e5dd59af5929c6abfdac329352c9a741581508
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-07-12 13:23:17 +0500

    added tests for rendering loader & note items when needed

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -6,4 +6,45 @@ describe('Home Page tests', () => {
     cy.visit('/auth');
     cy.login('dummyUser@gmail.com', '123456');
   });
+
+  it('redirects to Home Page upon successful login', () => {
+    cy.url().should('include', '/');
+    cy.contains('My Notes').should('exist');
+  });
+
+  it('displays loader when fetching notes', () => {
+    cy.intercept('GET', 'http://localhost:5000/api/notes', {
+      statusCode: 200,
+      body: [],
+      delay: 1000,
+    }).as('mockGetResponse');
+
+    cy.get('[data-testid="loader"]').should('exist');
+    cy.wait('@mockGetResponse');
+    cy.get('[data-testid="loader"]').should('not.exist');
+  });
+
+  it('displays the notes on the home page', () => {
+    cy.intercept('GET', 'http://localhost:5000/api/notes', {
+      statusCode: 200,
+      body: [
+        {
+          title: 'First Note',
+          description: 'This is the first note',
+        },
+        {
+          title: 'Second Note',
+          description: 'This is the second note',
+        },
+      ],
+    }).as('mockGetResponse');
+
+    cy.wait('@mockGetResponse').then((interceptor) => {
+      expect(interceptor.response.statusCode).to.eq(200);
+      expect(interceptor.response.body).to.have.length(2);
+    });
+
+    cy.get('[value="First Note"]').should('exist');
+    cy.get('[value="Second Note"]').should('exist');
+  });
 });
