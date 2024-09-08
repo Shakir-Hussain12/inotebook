0067c40dd6dd52f2cca32d76bd7ee2023d7df828
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-09-08 11:51:26 +0500

    Refactoring

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -18,65 +18,63 @@ const NoteItem = ({
   const [noteTag, setNoteTag] = useState(tag);
 
   return (
-    <article className="rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8" data-testid="noteItem">
-      <div className="flex items-start justify-between flex-wrap">
-        <div className="flex sm:gap-8">
-          <div
-            className="hidden sm:grid sm:size-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-indigo-500"
-            aria-hidden="true"
-          >
-            <div className="flex items-center gap-1">
-              <span className="h-8 w-0.5 rounded-full bg-indigo-500" />
-              <span className="h-6 w-0.5 rounded-full bg-indigo-500" />
-              <span className="h-4 w-0.5 rounded-full bg-indigo-500" />
-              <span className="h-6 w-0.5 rounded-full bg-indigo-500" />
-              <span className="h-8 w-0.5 rounded-full bg-indigo-500" />
-            </div>
-          </div>
-
-          <div>
-            <select
-              data-testid="tagEdit"
-              className="rounded border border-indigo-500 bg-indigo-500 px-3 py-1.5 text-[10px] font-medium text-white text-wrap"
-              disabled={!isEditable}
-              onChange={(e) => setNoteTag(e.target.value)}
-              defaultValue={noteTag}
-            >
-              <option value="General">General</option>
-              <option value="Work">Work</option>
-              <option value="Personal">Personal</option>
-            </select>
-
-            <h3 className="mt-4 text-lg font-medium sm:text-xl text-wrap">
-              <input
-                data-testid="titleEdit"
-                type="text"
-                className="bg-white"
-                defaultValue={noteTitle}
-                disabled={!isEditable}
-                onChange={(e) => setNoteTitle(e.target.value)}
-                ref={inputRef}
-              />
-            </h3>
-
-            <textarea
-              data-testid="descriptionEdit"
-              className="text-sm text-gray-700 resize-none min-w-full min-h-36 mt-3 bg-white text-wrap"
-              disabled={!isEditable}
-              onChange={(e) => setNoteDescription(e.target.value)}
-              defaultValue={noteDescription}
-            />
+    <div className="flex items-start justify-between flex-wrap">
+      <div className="flex sm:gap-8">
+        <div
+          className="hidden sm:grid sm:size-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-indigo-500"
+          aria-hidden="true"
+        >
+          <div className="flex items-center gap-1">
+            <span className="h-8 w-0.5 rounded-full bg-indigo-500" />
+            <span className="h-6 w-0.5 rounded-full bg-indigo-500" />
+            <span className="h-4 w-0.5 rounded-full bg-indigo-500" />
+            <span className="h-6 w-0.5 rounded-full bg-indigo-500" />
+            <span className="h-8 w-0.5 rounded-full bg-indigo-500" />
           </div>
         </div>
-        <Buttons
-          data={{
-            tag: noteTag, title: noteTitle, description: noteDescription, _id,
-          }}
-          inputRef={inputRef}
-          isEditable={isEditable}
-        />
+
+        <div>
+          <select
+            data-testid="tagEdit"
+            className="rounded border border-indigo-500 bg-indigo-500 px-3 py-1.5 text-[10px] font-medium text-white text-wrap"
+            disabled={!isEditable}
+            onChange={(e) => setNoteTag(e.target.value)}
+            defaultValue={noteTag}
+          >
+            <option value="General">General</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+          </select>
+
+          <h3 className="mt-4 text-lg font-medium sm:text-xl text-wrap">
+            <input
+              data-testid="titleEdit"
+              type="text"
+              className="bg-white"
+              defaultValue={noteTitle}
+              disabled={!isEditable}
+              onChange={(e) => setNoteTitle(e.target.value)}
+              ref={inputRef}
+            />
+          </h3>
+
+          <textarea
+            data-testid="descriptionEdit"
+            className="text-sm text-gray-700 resize-none min-w-full min-h-36 mt-3 bg-white text-wrap"
+            disabled={!isEditable}
+            onChange={(e) => setNoteDescription(e.target.value)}
+            defaultValue={noteDescription}
+          />
+        </div>
       </div>
-    </article>
+      <Buttons
+        data={{
+          tag: noteTag, title: noteTitle, description: noteDescription, _id,
+        }}
+        inputRef={inputRef}
+        isEditable={isEditable}
+      />
+    </div>
   );
 };
 
